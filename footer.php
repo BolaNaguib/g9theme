a3e777ecfaa3df5c3a134965dc99f9c2e0c0cefc
Bola Naguib <bolanaguib@gmail.com>
2019-04-22 04:34:47 +0200

    updated pwa

--- footer.php
+++ footer.php
@@ -35,5 +35,7 @@
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
+<?php wp_footer(); ?>
+
 </body>
 </html>
