b941f0fb501f26fa4f80f59146bc49f8a3a45bfa
bola naguib <bolanaguib@gmail.com>
2019-05-12 22:01:07 +0200

    updated cta

--- src/template/partials/section-cta.php
+++ src/template/partials/section-cta.php
@@ -1,9 +1,8 @@
 <section id="<?php the_sub_field('section_id'); ?>" class="section section_theme_<?php the_sub_field('section_theme'); ?>">
   <div class="uk-container uk-text-center">
-    <h3> <?php the_sub_field('before_title'); ?>&nbsp;
-      <a href="#"><?php echo get_bloginfo( 'name' ); ?>&nbsp;<?php the_field('city') ?> </a>&nbsp;
+    <h3>
       <?php the_sub_field('after_title'); ?>&nbsp;
-      <a href="tel:+<?php the_field('phone_call') ?>"><?php the_field('phone_display') ?>&nbsp;</a><?php the_sub_field('after_number'); ?></h3>
+      <a href="tel:+<?php the_field('phone_call') ?>"><?php the_field('phone_display') ?>&nbsp;</a><?php the_sub_field('after_number'); ?>&nbsp;<?php the_field('city') ?> </h3>
 
   </div>
 </section>
