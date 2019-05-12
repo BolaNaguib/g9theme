a269939d72f5911441f5a5a523d70583db88c89f
bola naguib <bolanaguib@gmail.com>
2019-05-12 20:59:58 +0200

    added cta

--- src/template/partials/section-hero.php
+++ src/template/partials/section-hero.php
@@ -11,5 +11,17 @@
       <?php endif; ?>
     <h1 class="hero__title"> <?php the_sub_field('hero_title'); ?> </h1>
     <p > <?php the_sub_field('hero_caption'); ?></p>
+    <?php if (get_sub_field('call_back_button')): ?>
+      <a style="background-color: #ffc800;
+    padding: 10px 20px;
+    border-radius: 10px;
+    color: #1c1f2a;
+    box-shadow: 5px 5px 5px 3px #00000069;
+    margin: 10px 0px;
+    display: -webkit-inline-box;" href="tel:<?php the_field('phone_call') ?>">        <?php the_field('phone_display', 'option'); ?>
+</a>
+<?php else : ?>
+
+<?php endif; ?>
   </div>
 </section>
