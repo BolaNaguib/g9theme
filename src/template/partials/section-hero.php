d27140df453dac40ab4b40f641149ea552111893
bola naguib <bolanaguib@gmail.com>
2019-05-12 21:17:35 +0200

    dsadsa

--- src/template/partials/section-hero.php
+++ src/template/partials/section-hero.php
@@ -14,13 +14,15 @@
     <h1 class="hero__title"> <?php the_sub_field('hero_title'); ?> </h1>
     <p > <?php the_sub_field('hero_caption'); ?></p>
     <?php if (get_sub_field('call_back_button')): ?>
-      <a style="background-color: #ffc800;
-    padding: 10px 20px;
+      <a style="    background-color: #1c1f2a;
+    padding: 15px 30px;
     border-radius: 10px;
-    color: #1c1f2a;
+    color: #ffffff;
     box-shadow: 5px 5px 5px 3px #00000069;
+    font-weight: bold;
     margin: 10px 0px;
-    display: -webkit-inline-box;" href="tel:<?php if (get_field('phone_call')): ?>
+    display: -webkit-inline-box;
+    font-size: 24px;" href="tel:<?php if (get_field('phone_call')): ?>
         <?php the_field('phone_call') ?>
         <?php else: ?>
           <?php the_field('phone', 'option'); ?>
