f7e2510bc9c4c783c3631d3804483a9b4c5de9b3
bola naguib <bolanaguib@gmail.com>
2019-05-13 17:47:42 +0200

    aaaa

--- src/template/footer.php
+++ src/template/footer.php
@@ -33,6 +33,7 @@
 
 </section>
 <!-- UIkit JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
 <?php wp_footer(); ?>
