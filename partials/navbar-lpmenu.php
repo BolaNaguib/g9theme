c76e50ba93ec6e7694b132149e14e329e563deea
Bola Naguib <bolanaguib@gmail.com>
2019-05-14 15:01:53 +0200

    updated menu lp

--- partials/navbar-lpmenu.php
+++ partials/navbar-lpmenu.php
@@ -1,69 +1,37 @@
 <div class="uk-navbar-right">
-<!-- START head_menu -->
-  <?php if (have_rows('head_menu', 'option')): ?>
+  <?php wp_nav_menu([
+           'theme_location'  => '',
+             'menu'            => '',
+             'container'       => '',
+             'container_class' => '',
+             'container_id'    => '',
+             'menu_class'      => '',
+             'menu_id'         => '',
+             'echo'            => true,
+             'fallback_cb'     => '',
+             'before'          => '',
+             'after'           => '',
+             'link_before'     => '',
+             'link_after'      => '',
+             'items_wrap'      => '<ul id="%1$s" class="%2$s uk-navbar-nav">%3$s</ul>',
+             'depth'           => 0,
+             'walker'          => new Bootstrap_Walker_Menu_Nav(),
 
-  <ul class="uk-navbar-nav">
+           ]); ?>
+  <ul class="uk-navbar-nav">             <li><a class=" button_type_toggle clickopen uk-visible@m" uk-toggle>
+    <span class="" uk-navbar-toggle-icon>
 
-    <?php while (have_rows('head_menu', 'option')): the_row(); ?>
+    </span>
+  </a>
+</li>
 
-      <li class="uk-visible@m dxd " style="display:none;"><a class="nav__link" href="<?php the_sub_field('nav_link'); ?>">
-          <?php the_sub_field('nav_title'); ?></a>
-        <?php
-          // check if the flexible content field has rows of data
-          //  START sub_navs
-          if (have_rows('sub_navs')):?>
-                <div class="uk-width-expand nav_theme_midnight" uk-dropdown>
-                  <div class="uk-dropdown-grid">
-                    <?php
-          // loop through the rows of data
-          while (have_rows('sub_navs')) : the_row();
-          if (get_row_layout() == 'sub_nav'):?>
-                    <div class="uk-margin-large">
-              <h3>
-                <?php the_sub_field('nav_block_title'); ?>
-              </h3>
-              <hr>
-              <?php if (have_rows('nav_block')):
-
-                ?>
-
-              <ul class="uk-nav uk-dropdown-nav uk-child-width-1-4" uk-grid>
-
-          <?php while (have_rows('nav_block')): the_row(); ?>
-                <li class="uk-margin-remove">
-                  <a class="nav__link nav__link_type_city uk-button-text" href="<?php the_sub_field('nav_link_url'); ?>">
-                    <?php the_sub_field('nav_link_title'); ?></a></li>
-
-                <?php endwhile; ?>
-    </ul>
-              <?php endif; ?>
-            </div>
-            <?php endif; endwhile;?>
-          </div>
-        </div>
-        <?php
-  endif; // END sub_navs?>
-      </li>
-
-    <?php endwhile; ?>
-
-                <li><a class=" button_type_toggle clickopen uk-visible@m" uk-toggle>
+                <li><a href="#offcanvas-nav" class=" button_type_toggle  uk-hidden@m" uk-toggle>
                     <span class="" uk-navbar-toggle-icon>
 
                     </span>
                   </a>
                 </li>
-
-                                <li><a href="#offcanvas-nav" class=" button_type_toggle  uk-hidden@m" uk-toggle>
-                                    <span class="" uk-navbar-toggle-icon>
-
-                                    </span>
-                                  </a>
-                                </li>
-  </ul>
-
-<?php endif; ?><!-- END head_menu -->
-
+           </ul>
 
 
 </div>
