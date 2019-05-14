fa9e5f52c11bdc35fc716e2089cba9fb75a57ea4
bola naguib <bolanaguib@gmail.com>
2019-05-14 13:04:18 +0200

    updated copy rights

--- footer.php
+++ footer.php
@@ -32,6 +32,18 @@
 </div>
 
 </section>
+<section class="uk-section section_theme_midnight">
+  <div class="uk-container">
+    <div class="uk-child-width-1-2@m uk-child-width-1-1" uk-grid>
+      <div class="uk-text-left@m uk-text-center">
+        © 2019 Garage Doors Tech LLC
+      </div>
+      <div class="uk-text-right@m uk-text-center">
+        Made with  <i style="color:red" class="fas fa-heart"></i> By <a target="_blank" href="bolanaguib.com"> Bolanaguib.com</a>
+      </div>
+    </div>
+  </div>
+</section>
 <!-- UIkit JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script async src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
