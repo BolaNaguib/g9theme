f8a05d5ed63c64b4ae4fcaae555a21f5cc31444d
bola naguib <bolanaguib@gmail.com>
2019-05-14 15:58:08 +0200

    updated ASDASD MENU

--- partials/navbar-menu.php
+++ partials/navbar-menu.php
@@ -45,7 +45,7 @@
                  'after'           => '',
                  'link_before'     => '',
                  'link_after'      => '',
-                 'items_wrap'      => '<ul id="%1$s" class="%2$s uk-navbar-nav">%3$s</ul>',
+                 'items_wrap'      => '<ul id="%1$s" class="%2$s uk-nav">%3$s</ul>',
                  'depth'           => 0,
                  'walker'          => new Bootstrap_Walker_Menu_Mobile(),
 
