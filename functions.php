d925c9b0d0e647f362d9e8e166f25ca40f08e937
bola naguib <bolanaguib@gmail.com>
2019-05-15 14:08:45 +0200

    [][][]

--- functions.php
+++ functions.php
@@ -38,10 +38,10 @@ if( function_exists('acf_add_options_page') ) {
 	// var $tree_type = array( 'post_type', 'taxonomy', 'custom' );
 	// var $db_fields = array( 'parent' => 'menu_item_parent', 'id' => 'db_id' );
 	function start_lvl( &$output, $depth = 0, $args = array() ) {
-		if ($depth == 0) $output .= '<div class="uk-navbar-dropdown nav_theme_midnight uk-padding-small"><ul class="uk-nav-sub">';
+		if ($depth == 0) $output .= '<ul class="uk-nav-sub">';
 	}
 	function end_lvl( &$output, $depth = 0, $args = array() ) {
-		if ($depth == 0) $output .= '</ul></div>';
+		if ($depth == 0) $output .= '</ul>';
 	}
 	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
 		if ($depth > 1) return;
