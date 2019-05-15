938174434d1098e1c262394c0aebd702703b1aa6
bola naguib <bolanaguib@gmail.com>
2019-05-15 15:52:51 +0200

    rerrrr

--- src/template/partials/section-contactform.php
+++ src/template/partials/section-contactform.php
@@ -3,8 +3,12 @@
 <div class="uk-container">
   <div class="uk-child-width-1-2@m uk-child-width-1-1 uk-flex uk-flex-middle" uk-grid>
     <div class="">
-      <h3 class="section__title"> <?php the_sub_field('card_title'); ?> </h3>
-      <hr class="hr_type_section <?php if( get_sub_field('section_theme') !== 'sunflower' ): ?> hr_theme_sunflower <?php endif; ?> ">
+      <h3 class="section__title uk-text-right@m uk-text-center"> <?php the_sub_field('card_title'); ?> </h3>
+      <div class=" uk-text-right@m uk-text-center">
+        <hr class="hr_type_section <?php if( get_sub_field('section_theme') !== 'sunflower' ): ?> hr_theme_sunflower <?php endif; ?> ">
+
+      </div>
+
       <p> <?php the_sub_field('card_caption'); ?> </p>    </div>
     <div class="">
         <div class="uk-card-default card_type_form">
