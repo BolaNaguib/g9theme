8cdca60e9108e584a2e5ac791eb8471fbf33373e
Bola Naguib <bolanaguib@gmail.com>
2019-05-18 15:48:59 +0200

    [ XVXlabs]

--- footer.php
+++ footer.php
@@ -42,7 +42,7 @@
         © 2019 Garage Doors Tech LLC
       </div>
       <div class="uk-text-right@m uk-text-center">
-        Made with  <i style="color:red" class="fas fa-heart"></i> By <a target="_blank" href="vxvlabs.com"> <img alt="vxvlabx" height="100" src=" <?php echo get_directory_uri(); ?> /images/vxvlogo.png" /></a>
+        Made with  <i style="color:red" class="fas fa-heart"></i> By <a target="_blank" href="vxvlabs.com"> <img alt="vxvlabx" height="100" src=" <?php echo get_template_directory_uri(); ?> /images/vxvlogo.png" /></a>
       </div>
     </div>
   </div>
